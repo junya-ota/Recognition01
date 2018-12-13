ee209a70019c057ce30b46bef63ff4829ee1a8ba
1618019 <1618019@cs16019>
2018-12-13 16:14:14 +0900

    2018/12/13 応用コンピュータシステム実験にて作成。

--- src/recognition01/Recognition01_main.java
+++ src/recognition01/Recognition01_main.java
@@ -11,25 +11,9 @@ import com.ibm.watson.developer_cloud.visual_recognition.v3.model.DetectedFaces;
 public class Recognition01_main {
 
 	public static void main(String[] args) {
-		VisualRecognition service = new VisualRecognition("2018-03-19");
-		IamOptions iamOptions = new IamOptions.Builder()
-		  .apiKey("834iufuheuhefh9f43hjfj98yg3jh38yYYIQWHF9hf899f")
-		  .build();
-		service.setIamCredentials(iamOptions);
-
-		DetectFacesOptions detectFacesOptions = null;
-		try {
-			detectFacesOptions = new DetectFacesOptions.Builder()
-					  .imagesFile(new File("img/img_5008f1cd72f6318332f53b2654a2f3be397279.jpg"))
-					  .build();
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		DetectedFaces result = service.detectFaces(detectFacesOptions).execute();
-			System.out.println(result);
-
+		Recognition01_lib rlib = new Recognition01_lib();
+		
+		rlib.getResult("img/img_5008f1cd72f6318332f53b2654a2f3be397279.jpg");
 
 
 	}
